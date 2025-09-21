7fd886ce4577f7a8ec73442ca494962f88769858
samm <samm@blah.net>
2025-09-20 21:11:50 -0400

    updated to shoot. a stream for now

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
+import sys
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 def main():
     pygame.init()
     gameClock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     myPlayer = Player( x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt) 
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(myPlayer):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
-        myPlayer.draw(screen)
-        pygame.display.flip()
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()  
 
         dt = gameClock.tick(60) / 1000
+        
 
     
 
